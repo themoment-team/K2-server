6ccdda03d1617bbac621be622dfd1990a64d61e7
johnjihwan <s20062@gsm.hs.kr>
2021-02-12 00:01:12 +0900

    [UPDATE]Set the basic development environment.

---
+++ src/main/java/com/moment/the/TheApplication.java
@@ -0,0 +1,13 @@
+package com.moment.the;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheApplication.class, args);
+	}
+
+}
