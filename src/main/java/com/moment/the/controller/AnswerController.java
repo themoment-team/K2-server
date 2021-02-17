880ec29ccb2b3d7a649df39d8ea4840206531084
siwony_ <siwon103305@gmail.com>
2021-02-18 00:29:51 +0900

    [UPDATE]AnswerContoller save method

--- src/main/java/com/moment/the/controller/AnswerController.java
+++ src/main/java/com/moment/the/controller/AnswerController.java
@@ -4,9 +4,11 @@ import com.moment.the.dto.AnswerDto;
 import com.moment.the.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/v1/top10")
+@RestController
+@RequestMapping("/v1/top10")
 @RequiredArgsConstructor
 public class AnswerController {
 
