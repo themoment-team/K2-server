58c052d39c29339fbb6b1152b474b9315ba76c4a
siwony_ <siwon103305@gmail.com>
2021-02-18 00:15:16 +0900

    [ADD]AnswerService save method

--- src/main/java/com/moment/the/service/AnswerService.java
+++ src/main/java/com/moment/the/service/AnswerService.java
@@ -1,2 +1,17 @@
-package com.moment.the.service;public class AnswerService {
+package com.moment.the.service;
+
+import com.moment.the.dto.AnswerDto;
+import com.moment.the.repository.AnswerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+
+    final private AnswerRepository answerRepo;
+
+    public void save(AnswerDto answerDto){
+        answerRepo.save(answerDto.toEntity());
+    }
 }
