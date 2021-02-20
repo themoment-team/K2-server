e81809ed80c4a7b451588e5154f8f1f0c0e4f60f
johnjihwan <s20062@gsm.hs.kr>
2021-02-20 22:02:19 +0900

    [UPDATE]ALL JWT TOKEN INITICIAL COMMIT

--- src/main/java/com/moment/the/domain/AdminDomain.java
+++ src/main/java/com/moment/the/domain/AdminDomain.java
@@ -47,32 +47,26 @@ public class AdminDomain implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
-
     @Override
     public String getPassword() {
         return getPassword();
     }
-
     @Override
     public String getUsername() {
         return getAdminId();
     }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
     @Override
     public boolean isEnabled() {
         return true;
