1d2747214cc6c56536add75daffa912aa0322165
johnjihwan <s20062@gsm.hs.kr>
2021-03-03 18:28:49 +0900

    [UPDATE]initial commit

--- src/main/java/com/moment/the/repository/AdminRepository.java
+++ src/main/java/com/moment/the/repository/AdminRepository.java
@@ -1,8 +1,8 @@
 package com.moment.the.repository;
 
 import com.moment.the.domain.AdminDomain;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminRepository extends CrudRepository<AdminDomain, Long> {
+public interface AdminRepository extends JpaRepository<AdminDomain, Long> {
     AdminDomain findByAdminId(String adminId);
 }
