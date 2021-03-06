5fe0e55246cb62b2eeb3132f15bb74eb15b73221
johnjihwan <s20062@gsm.hs.kr>
2021-03-06 23:04:13 +0900

    [UPDATE] Auth Config complete.

--- src/main/java/com/moment/the/repository/AdminRepository.java
+++ src/main/java/com/moment/the/repository/AdminRepository.java
@@ -3,8 +3,7 @@ package com.moment.the.repository;
 import com.moment.the.domain.AdminDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<AdminDomain, Long> {
-    Optional<AdminDomain> findByAdminId(String adminId);
+    AdminDomain findByAdminId(String adminId);
 }
