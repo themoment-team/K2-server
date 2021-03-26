899c2dd582617a7310074b6a5000d02fea9032c3
johnjihwan <s20062@gsm.hs.kr>
2021-03-27 02:35:57 +0900

    [UPDATE]Response group package

---
+++ src/main/java/com/moment/the/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.moment.the.controller;
+
+import com.moment.the.domain.response.CommonResult;
+import com.moment.the.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1")
+@RequiredArgsConstructor
+public class AdminController {
+
+
+}
---
+++ src/main/java/com/moment/the/domain/response/ListResult.java
@@ -0,0 +1,12 @@
+package com.moment.the.domain.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ListResult<T> extends CommonResult {
+    private List<T> list;
+}
