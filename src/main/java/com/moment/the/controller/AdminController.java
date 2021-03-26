64761367446fd56a19a562ac516ab9106ea5e173
johnjihwan <s20062@gsm.hs.kr>
2021-03-27 03:42:36 +0900

    [UPDATE]AdminController complete

--- src/main/java/com/moment/the/controller/AdminController.java
+++ src/main/java/com/moment/the/controller/AdminController.java
@@ -2,9 +2,13 @@ package com.moment.the.controller;
 
 import com.moment.the.config.security.JwtUtil;
 import com.moment.the.domain.AdminDomain;
+import com.moment.the.domain.response.CommonResult;
+import com.moment.the.domain.response.ResponseService;
+import com.moment.the.dto.AdminDto;
 import com.moment.the.dto.SignInDto;
 import com.moment.the.repository.AdminRepository;
 import com.moment.the.service.AuthService;
+import com.moment.the.service.AuthServiceImpl;
 import com.moment.the.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,31 +16,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
 public class AdminController {
-    private final AdminRepository adminRepository;
     private final AuthService authService;
     private final JwtUtil jwtUtil;
     private final RedisUtil redisUtil;
+    private final ResponseService responseService;
+
     @PostMapping("/login")
-    public String login(@RequestBody SignInDto signInDto,
-                        HttpServletResponse response,
-                        HttpServletRequest request) throws Exception {
-        try {
-            final AdminDomain adminDomain = authService.loginUser(signInDto.getAdminId(), signInDto.getAdminPwd());
-            final String token = jwtUtil.generateToken(adminDomain);
-            final String refreshJwt = jwtUtil.generateRefreshToken(adminDomain);
-            redisUtil.setDataExpire(adminDomain.getAdminId(), refreshJwt, JwtUtil.REFRESH_TOKEN_VALIDATION_SECOND);
-
-        }catch (Exception e){
-
-        }
-        return "";
+    public CommonResult login(@RequestBody SignInDto signInDto) throws Exception {
+        final AdminDomain adminDomain = authService.loginUser(signInDto.getAdminId(), signInDto.getAdminPwd());
+        final String token = jwtUtil.generateToken(adminDomain);
+        final String refreshJwt = jwtUtil.generateRefreshToken(adminDomain);
+        redisUtil.setDataExpire(adminDomain.getAdminId(), refreshJwt, JwtUtil.REFRESH_TOKEN_VALIDATION_SECOND);
+        return responseService.getSuccessResult();
     }
 
+    @PostMapping("/signup")
+    public CommonResult signup(@RequestBody AdminDto adminDto){
+        authService.signUp(adminDto);
+        return responseService.getSuccessResult();
+    }
 }
