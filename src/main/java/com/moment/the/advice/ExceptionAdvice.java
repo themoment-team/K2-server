4848811ba502faf8f97c902125ea54b2d538704e
johnjihwan <s20062@gsm.hs.kr>
2021-03-31 10:32:42 +0900

    [UPDATE]Exception Message Config add

--- src/main/java/com/moment/the/advice/ExceptionAdvice.java
+++ src/main/java/com/moment/the/advice/ExceptionAdvice.java
@@ -1,4 +1,11 @@
 package com.moment.the.advice;
 
+import com.moment.the.domain.response.ResponseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RequiredArgsConstructor
+@RestControllerAdvice
 public class ExceptionAdvice {
+    private final ResponseService responseService;
 }
