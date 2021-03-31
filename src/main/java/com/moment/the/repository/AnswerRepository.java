acb2803721520f061a62078cc86c361c5c936134
siwony_ <siwon103305@gmail.com>
2021-03-31 12:01:55 +0900

    [UPDATE]implement findByAdminDomain at AnswerRepository

--- src/main/java/com/moment/the/repository/AnswerRepository.java
+++ src/main/java/com/moment/the/repository/AnswerRepository.java
@@ -1,13 +1,17 @@
 package com.moment.the.repository;
 
+import com.moment.the.domain.AdminDomain;
 import com.moment.the.domain.AnswerDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<AnswerDomain, Long> {
     List<AnswerDomain> findAllBy();
     void deleteAllByAnswerIdx(Long answerIdx);
+
+    Optional<AnswerDomain> findByAdminDomain(AdminDomain adminDomain);
 }
