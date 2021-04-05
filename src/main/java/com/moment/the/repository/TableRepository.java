36dda46792b828d64f5482e01d070158ee923ae6
johnjihwan <s20062@gsm.hs.kr>
2021-04-05 20:46:40 +0900

    [UPDATE]Fuxx that shit Constraints

--- src/main/java/com/moment/the/repository/TableRepository.java
+++ src/main/java/com/moment/the/repository/TableRepository.java
@@ -2,7 +2,6 @@ package com.moment.the.repository;
 
 import com.moment.the.domain.TableDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
