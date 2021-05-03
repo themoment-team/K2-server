b281779975e5625a6b4b2d11750305658757a258
siwony_ <siwon103305@gmail.com>
2021-05-03 17:19:38 +0900

    [FIX] v1/uncomfortable omission goods

--- src/main/java/com/moment/the/service/TableService.java
+++ src/main/java/com/moment/the/service/TableService.java
@@ -4,7 +4,6 @@ import com.moment.the.advice.exception.GoodsNotCancelException;
 import com.moment.the.advice.exception.NoPostException;
 import com.moment.the.domain.TableDomain;
 import com.moment.the.dto.TableDto;
-import com.moment.the.dto.TableViewAllDto;
 import com.moment.the.repository.TableRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,11 +29,8 @@ public class TableService {
     }
 
     // 전체 페이지 보여주기.
-    public List<TableViewAllDto> viewAll(){
-        ModelMapper modelMapper = new ModelMapper();
-        return tableRepository.findAllByOrderByBoardIdxDesc().stream()
-                .map(m -> modelMapper.map(m, TableViewAllDto.class))
-                .collect(Collectors.toList());
+    public List<TableDomain> viewAll(){
+        return tableRepository.findAllByOrderByBoardIdxDesc();
     }
 
     // 좋아요 수 증가.
