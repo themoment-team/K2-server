1ea3c30e23d2e20d42785b7d8d0cd679a963846d
siwony_ <siwon103305@gmail.com>
2021-05-10 21:13:21 +0900

    [UPDATE] tableViewTopBy finish

--- src/main/java/com/moment/the/repository/TableRepository.java
+++ src/main/java/com/moment/the/repository/TableRepository.java
@@ -1,13 +1,12 @@
 package com.moment.the.repository;
 
 import com.moment.the.domain.TableDomain;
-import com.moment.the.dto.TableDto;
 import com.moment.the.dto.TableViewDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,4 +25,10 @@ public interface TableRepository extends JpaRepository<TableDomain, Long>{
             "ORDER BY table.boardIdx DESC "
     )
     List<TableViewDto> tableViewAll();
+
+    @Query("SELECT new com.moment.the.dto.TableViewDto(table.boardIdx, table.content, table.goods, answer)" +
+            "FROM TableDomain table LEFT JOIN table.answerDomain answer " +
+            "ORDER BY table.goods DESC "
+    )
+    List<TableViewDto> tableViewTopBy(Pageable p);
 }
