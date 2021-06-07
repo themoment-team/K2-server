c53c6e81eb1bebe699143e0614ef8af44833cc09
johnjihwan <s20062@gsm.hs.kr>
2021-06-07 17:49:25 +0900

    I'm committed to security vulnerabilities.

--- src/main/java/com/moment/the/config/mvc/WebMvcConfigurerImpl.java
+++ src/main/java/com/moment/the/config/mvc/WebMvcConfigurerImpl.java
@@ -10,7 +10,7 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청에 대해서
-                .allowedOrigins("https://the-moment.org", "https://www.the-moment.org") // local, docker
+                .allowedOrigins("https://the-moment.org", "https://www.the-moment.org", "https://server.the-moment.org") // local, docker
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
