4be965f849c32c51622e5c4b264f04343c34c151
johnjihwan <s20062@gsm.hs.kr>
2021-06-09 16:17:35 +0900

    회원가입 로직 인증 로직 테스트코드 작성

--- src/test/java/com/moment/the/TheApplicationTests.java
+++ src/test/java/com/moment/the/TheApplicationTests.java
@@ -2,8 +2,17 @@ package com.moment.the;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.moment.the.domain.AdminDomain;
+import com.moment.the.dto.AdminDto;
+import com.moment.the.repository.AdminRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class TheApplicationTests {
@@ -17,5 +26,29 @@ class TheApplicationTests {
 		//then
 		assertEquals(expected, actual);
 		System.out.println("expected: " + expected + " actual: " + actual);
+	}
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	@Autowired
+	private AdminRepository adminRepository;
+
+	@Test
+	void 회원가입(AdminDto adminDto){
+		//Given
+		String email = "s20062@gsm.hs.kr";
+		String adminName = "jihwan";
+		String pw = "1234";
+
+		//when
+		adminDto.setAdminPwd(passwordEncoder.encode(pw));
+		adminDto.setAdminId(email);
+		adminDto.setAdminName(adminName);
+		adminRepository.save(adminDto.toEntity());
+
+		//then
+		assertEquals(adminDto.getAdminId(), email);
+		assertEquals(passwordEncoder.matches(pw,adminDto.getAdminPwd()), true);
+		assertEquals(adminDto.getAdminName(), "jihwan");
 	}
 }
