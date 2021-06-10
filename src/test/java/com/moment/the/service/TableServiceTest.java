f40ac999492b6f7d2a7843843c25ea544ce94a06
siwony_ <siwon103305@gmail.com>
2021-06-10 13:57:28 +0900

    [UPDATE] TableService top30 검증 test

--- src/test/java/com/moment/the/service/TableServiceTest.java
+++ src/test/java/com/moment/the/service/TableServiceTest.java
@@ -2,11 +2,17 @@ package com.moment.the.service;
 
 import com.moment.the.domain.TableDomain;
 import com.moment.the.dto.TableDto;
+import com.moment.the.dto.TableViewDto;
 import com.moment.the.repository.TableRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,9 +35,34 @@ class TableServiceTest {
         tableRepo.flush();
         TableDomain savedTable = tableRepo.findByBoardIdx(writeTable.getBoardIdx()).orElseThrow(() -> new IllegalArgumentException("Table을 찾을 수 없습니다. (테스트실패)"));
 
-        // than
+        // then
         assertEquals(tableDto.getContent(), savedTable.getContent());
     }
 
+    @Test
+    @DisplayName("Table top30 보여주기 테스트")
+    void Table_top30_view_검증(){
+        // Given
+        AtomicInteger i = new AtomicInteger(1);
+        List<TableDomain> TableDomains = Stream.generate(
+                () -> TableDomain.builder()
+                        .goods(i.getAndIncrement())
+                        .content("고양이는 귀여워")
+                        .build()
+        ).limit(40).collect(Collectors.toList());
+
+        // When
+        tableRepo.saveAll(TableDomains);
+        List<TableViewDto> viewTop30 = tableService.view();
+
+        // Then
+        assertEquals(viewTop30.size(), 30);
+        AtomicInteger j = new AtomicInteger(40);
+        Stream<TableViewDto> streamViewTop30 = viewTop30.stream();
+        for(TableViewDto v : viewTop30 ) {
+            assertEquals(v.getGoods(), j.getAndDecrement());
+        }
+        assertEquals(j.get(), 10);
+    }
 
 }
