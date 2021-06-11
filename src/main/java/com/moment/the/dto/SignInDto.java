cad36a7f43a16ee7a3da74cac8a850be7be44e0f
johnjihwan <s20062@gsm.hs.kr>
2021-06-11 15:35:11 +0900

    toEntity 메서드를 생성하고 생성자 옵션 어노테이션을 추가했습니다

--- src/main/java/com/moment/the/dto/SignInDto.java
+++ src/main/java/com/moment/the/dto/SignInDto.java
@@ -1,15 +1,24 @@
 package com.moment.the.dto;
 
-import lombok.Builder;
-import lombok.Getter;
+import com.moment.the.domain.AdminDomain;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
-@Getter
 @Builder
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignInDto {
     @NotBlank(message = "id should be valid")
     private String adminId;
     @NotBlank(message = "password should be valid")
     private String adminPwd;
+
+    private AdminDomain toEntity(){
+        return AdminDomain.builder()
+                .adminId(this.adminId)
+                .adminPwd(this.adminPwd)
+                .build();
+    }
 }
