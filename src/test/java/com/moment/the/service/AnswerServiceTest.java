7c4d38d702b1db8c4915be1124f2dc6f4341e720
siwony_ <siwon103305@gmail.com>
2021-06-13 02:59:33 +0900

    [FIX] save 검증

--- src/test/java/com/moment/the/service/AnswerServiceTest.java
+++ src/test/java/com/moment/the/service/AnswerServiceTest.java
@@ -8,16 +8,21 @@ import com.moment.the.dto.AdminDto;
 import com.moment.the.dto.AnswerDto;
 import com.moment.the.dto.TableDto;
 import com.moment.the.repository.AnswerRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,32 +36,58 @@ class AnswerServiceTest {
     @Autowired TableService tableService;
     @Autowired MyUserDetailsService userDetailsService;
 
+    // 유저정보
     final String ADMIN_ID = "email@email";
     final String PASSWORD = "password";
-    final String ROLE = "ADMIN";
+    final String ROLE = "ROLE_USER";
 
+    // test 편의를 위한 회원가입 매서드
+    void adminSignUp() throws Exception {
+        AdminDto adminDto = new AdminDto(ADMIN_ID, PASSWORD, "Admin");
+        adminService.signUp(adminDto);
+    }
+
+    //test 편의를 위한 로그인 매서드
+    AdminDomain adminLogin() throws Exception {
+        AdminDomain adminDomain = adminService.loginUser(ADMIN_ID, PASSWORD);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                adminDomain.getAdminId(),
+                adminDomain.getAdminPwd(),
+                List.of(new SimpleGrantedAuthority(ROLE)));
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(token);
+
+        return adminDomain;
+    }
 
     @Test @DisplayName("답변 작성하기 (save) 검증")
-//    @WithMockUser(username = ADMIN_ID, password = PASSWORD, authorities = ROLE)
-    @WithAnonymousUser
     void save_검증() throws Exception {
         // Given
-        AdminDto adminDto = new AdminDto(ADMIN_ID, PASSWORD, ADMIN_ID);
-        adminService.signUp(adminDto);
+        //회원가입
+        adminSignUp();
 
-        String tableContent = "급식이 맛이 없어요 급식에 질을 높여주세요!";
-        TableDto tableDto = new TableDto(tableContent);
+        //로그인
+        AdminDomain adminDomain = adminLogin();
+
+        //Table 등록
+        String TABLE_CONTENT = "급식이 맛이 없어요 급식에 질을 높여주세요!";
+        TableDto tableDto = new TableDto(TABLE_CONTENT);
         TableDomain tableDomain = tableService.write(tableDto);
 
-        String answerContent = "급식이 맛이 없는 이유는 삼식이라 어쩔수 없어요~";
-        AnswerDto answerDto = new AnswerDto(answerContent, null);
+        //answer 등록
+        String ANSWER_CONTENT = "급식이 맛이 없는 이유는 삼식이라 어쩔수 없어요~";
+        AnswerDto answerDto = new AnswerDto(ANSWER_CONTENT, null);
 
         // When
         answerService.save(answerDto, tableDomain.getBoardIdx());
         AnswerDomain savedAnswer = answerRepo.findTop1ByTableDomain_BoardIdx(tableDomain.getBoardIdx());
 
         // Then
+        assertEquals(savedAnswer.getAnswerContent(), ANSWER_CONTENT);
         assertEquals(savedAnswer.getTableDomain(), tableDomain);
+        assertEquals(savedAnswer.getAdminDomain(), adminDomain);
     }
+
+
 
 }
