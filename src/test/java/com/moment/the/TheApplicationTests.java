3e63f6921d9fbbadc055cc5a6755008f07ddf955
johnjihwan <s20062@gsm.hs.kr>
2021-06-14 10:00:31 +0900

    비밀번호를 잘못 입력했을때 exception이 터지는지 확인하는 테스트코드 검증 완료

--- src/test/java/com/moment/the/TheApplicationTests.java
+++ src/test/java/com/moment/the/TheApplicationTests.java
@@ -1,8 +1,5 @@
 package com.moment.the;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.moment.the.advice.exception.UserAlreadyExistsException;
 import com.moment.the.advice.exception.UserNotFoundException;
 import com.moment.the.domain.AdminDomain;
@@ -30,6 +27,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -211,5 +210,26 @@ class TheApplicationTests {
 		System.out.println(encodePw);
 		//then
 		assertEquals(passwordEncoder.matches(pw, encodePw), true);
+	}
+
+	@Test
+	void 서비스_토큰_발급(){
+		//Given
+		boolean exceptionCatched = false;
+
+		AdminDto adminDto = new AdminDto();
+		adminDto.setAdminId("admin@admin");
+		adminDto.setAdminPwd(passwordEncoder.encode("1234"));
+		adminRepository.save(adminDto.toEntity());
+
+		//When
+		try {
+			adminServiceImpl.loginUser("admin@admin", "134");
+		} catch (UserNotFoundException e) {
+			exceptionCatched = true;
+		}
+
+		//Then
+		assertTrue(exceptionCatched);
 	}
 }
