d5cbf7dd265c0208f5799354cc5539377276b03a
siwony_ <siwon103305@gmail.com>
2021-07-13 10:17:58 +0900

    [UPDATE] 정적 변수명 변경 및 isTokenExpired 로직 변경 앞으로 모든 token 검증 로직 변경 예정

--- src/main/java/com/moment/the/config/security/jwt/JwtUtil.java
+++ src/main/java/com/moment/the/config/security/jwt/JwtUtil.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -21,11 +20,11 @@ public class JwtUtil {
     public final static long ACCESS_TOKEN_EXPIRATION_TIME = 1000l * 3600 * 6;  // milli_sec * hour * 6 = 6hour
     public final static long REFRESH_TOKEN_EXPIRATION_TIME = 1000l * 3600 * 24 * 30 * 7;  // milli_sec X hour X day X month * 7 = 7month
 
-    final static public String ACCESS_TOKEN_NAME = "accessToken";
-    final static public String REFRESH_TOKEN_NAME = "refreshToken";
+    final static public String TYPE_OF_ACCESS_TOKEN = "accessToken";
+    final static public String TYPE_OF_REFRESH_TOKEN = "refreshToken";
 
-    final static public String TOKEN_CLAIM_NAME_USER_EMAIL = "userEmail"; // token의 user email 추출시 필요한 claims이름
-    final static public String TOKEN_CLAIM_NAME_TOKEN_TYPE = "tokenType";
+    final static public String TOKEN_CLAIM_NAME_FOR_USER_EMAIL = "userEmail"; // token의 user email 추출시 필요한 claims이름
+    final static public String TOKEN_CLAIM_NAME_FOR_TOKEN_TYPE = "tokenType";
 
     private Key getSigningKey(String secretKey) {
         byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
@@ -41,50 +40,44 @@ public class JwtUtil {
     }
 
     public String getUserEmail(String token){
-        return extractAllClaims(token).get(TOKEN_CLAIM_NAME_USER_EMAIL, String.class);
+        return extractAllClaims(token).get(TOKEN_CLAIM_NAME_FOR_USER_EMAIL, String.class);
     }
 
     public String getTokenType(String token){
-        return extractAllClaims(token).get(TOKEN_CLAIM_NAME_TOKEN_TYPE, String.class);
+        return extractAllClaims(token).get(TOKEN_CLAIM_NAME_FOR_TOKEN_TYPE, String.class);
     }
 
     public Boolean isTokenExpired(String token) {
-        final Date expiration = extractAllClaims(token).getExpiration();
-        return expiration.before(new Date());
+        try{
+            extractAllClaims(token).getExpiration();
+            return false;
+        }catch(ExpiredJwtException e){
+            return true;
+        }
     }
 
-    public String generateAccessToken(String email) {
-        return doGenerateToken(email, ACCESS_TOKEN_NAME, ACCESS_TOKEN_EXPIRATION_TIME);
-    }
-
-    public String generateRefreshToken(String email) {
-        return doGenerateToken(email, REFRESH_TOKEN_NAME, REFRESH_TOKEN_EXPIRATION_TIME);
+    public Boolean validateToken(final String token, final String tokenType) {
+        return !isTokenExpired(token) && getTokenType(token).equals(tokenType);
     }
 
     public String doGenerateToken(String userEmail, String tokenType, long expireTime) {
         final Claims claims = Jwts.claims();
         claims.put("userEmail", userEmail);
         claims.put("tokenType", tokenType);
-        String jwt = Jwts.builder()
+        return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expireTime))
                 .signWith(getSigningKey(SECRET_KEY), SignatureAlgorithm.HS256)
                 .compact();
-        return jwt;
     }
 
-    public Boolean validateGeneralToken(String token){
-        try{
-            extractAllClaims(token);
-        }catch (ExpiredJwtException e){
-            return false;
-        }
-        return !isTokenExpired(token);
+    public String generateAccessToken(String email) {
+        return doGenerateToken(email, TYPE_OF_ACCESS_TOKEN, ACCESS_TOKEN_EXPIRATION_TIME);
     }
 
-    public Boolean validateAccessToken(String token, UserDetails userDetails) {
-        final String username = getUserEmail(token);
-        return username.equals(userDetails.getUsername()) && !isTokenExpired(token) && getTokenType(token).equals(ACCESS_TOKEN_NAME);
+    public String generateRefreshToken(String email) {
+        return doGenerateToken(email, TYPE_OF_REFRESH_TOKEN, REFRESH_TOKEN_EXPIRATION_TIME);
     }
+
 }
