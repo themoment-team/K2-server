151c3f38a64454c80b9109fecfc084cc3cbc8e62
siwony_ <siwon103305@gmail.com>
2021-08-28 17:43:27 +0900

    [REMOVE] out디렉리가 ignore되지 않아 캐시삭제

--- src/test/java/com/moment/the/TheApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.moment.the;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-@Transactional
-class TheApplicationTests {
-}
