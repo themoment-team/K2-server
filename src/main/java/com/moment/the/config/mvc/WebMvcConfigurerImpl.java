7021f2a1bfc7e74a8803e24ac74aeac46706ee9f
siwony_ <siwon103305@gmail.com>
2021-09-23 21:00:09 +0900

    Update localhost origin allow

--- src/main/java/com/moment/the/config/mvc/WebMvcConfigurerImpl.java
+++ src/main/java/com/moment/the/config/mvc/WebMvcConfigurerImpl.java
@@ -10,7 +10,10 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청에 대해서
-                .allowedOrigins("https://the-moment.org", "https://www.the-moment.org", "https://server.the-moment.org") // local, docker
+                .allowedOrigins(
+                        "https://the-moment.org", "https://www.the-moment.org", "https://server.the-moment.org",
+                        "http://localhost:8080", "http://localhost:3000"
+                ) // local, docker
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
