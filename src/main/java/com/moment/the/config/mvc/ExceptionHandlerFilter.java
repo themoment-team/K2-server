503ec5887a29197a65411512ac023378c48ecb4c
siwony_ <siwon103305@gmail.com>
2021-09-24 09:34:22 +0900

    Update ExceptionHandlerFilter setExceptionRes메서드 에서 예외가 발생했을 떄 logging을 slf4j로 변경 + catch의 Exception을 정확히 명시함

--- src/main/java/com/moment/the/config/mvc/ExceptionHandlerFilter.java
+++ src/main/java/com/moment/the/config/mvc/ExceptionHandlerFilter.java
@@ -7,6 +7,7 @@ import com.moment.the.exceptionAdvice.exception.UserNotFoundException;
 import com.moment.the.response.ResponseService;
 import com.moment.the.response.result.CommonResult;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Slf4j
 @Component
@@ -34,7 +36,7 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
             setExceptionRes(HttpStatus.BAD_REQUEST, res, exceptionAdvice.accessTokenExpiredException(req, e));
         }catch (UserNotFoundException e){
             setExceptionRes(HttpStatus.BAD_REQUEST, res, exceptionAdvice.userNotFoundException(req, e));
-        } catch (Exception e){
+        }catch (Exception e){
             log.error("알 수 없는 에러 발생", e);
             setExceptionRes(HttpStatus.INTERNAL_SERVER_ERROR, res, exceptionAdvice.defaultException(req, e));
         }
@@ -50,14 +52,16 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         int exceptionCode = exceptionResult.getCode();
         String exceptionMsg = exceptionResult.getMsg();
 
-        try{
+        try {
             String exceptionResultToJson = resService.getFailResultConvertString(exceptionCode, exceptionMsg); // CommonResult 에 있는 값을 json 으로 변환
-            System.out.println(exceptionResultToJson);
-            res.getWriter().write(exceptionResultToJson); // filter 단에서 client 에 json 을 보넨다.
-        }catch (Exception e){
-            e.printStackTrace();
-            throw new UnknownError();
+            log.debug(
+                    "filter에서 Exception 발생 원인: \n {}",
+                    exceptionResultToJson
+            );
+            res.getWriter().write(exceptionResultToJson); // filter 단에서 client에 json를 보넨다.
+        } catch (IOException e) {
+            log.error("알 수 없는 에러 발생", e);
+            throw new RuntimeException();
         }
     }
-
 }
