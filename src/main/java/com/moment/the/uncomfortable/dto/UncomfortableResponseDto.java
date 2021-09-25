484fde97518aedee36c8a4b64bc0930c370f861a
siwony_ <siwon103305@gmail.com>
2021-09-25 20:12:32 +0900

    Update uncomfortableViewAll는 isAnswer를 반환하지 않아도된다.

--- src/main/java/com/moment/the/uncomfortable/dto/UncomfortableResponseDto.java
+++ src/main/java/com/moment/the/uncomfortable/dto/UncomfortableResponseDto.java
@@ -1,14 +1,22 @@
 package com.moment.the.uncomfortable.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
-@Builder
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Builder @Getter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) //null인 필드를 제외합니다.
 public class UncomfortableResponseDto {
 
-    private Long boardIdx;
+    private Long uncomfortableIdx;
     private String content;
     private int goods;
-    private boolean isAnswer;
+
+    private Boolean isAnswer; // rank API에서만 사용한다.
+
+    public UncomfortableResponseDto(Long uncomfortableIdx, String content, int goods){
+        this.uncomfortableIdx = uncomfortableIdx;
+        this.content = content;
+        this.goods = goods;
+    }
 }
