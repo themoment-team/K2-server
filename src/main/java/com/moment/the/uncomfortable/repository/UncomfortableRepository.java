ec39effd5e2b511e9c19ad8be1f0b6b72bf0c7de
siwony_ <siwon103305@gmail.com>
2021-09-25 21:28:52 +0900

    JPQL 레거시 청산 완료

--- src/main/java/com/moment/the/uncomfortable/repository/UncomfortableRepository.java
+++ src/main/java/com/moment/the/uncomfortable/repository/UncomfortableRepository.java
@@ -1,12 +1,9 @@
 package com.moment.the.uncomfortable.repository;
 
 import com.moment.the.uncomfortable.UncomfortableDomain;
-import com.moment.the.uncomfortable.dto.UncomfortableResponseDto;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UncomfortableRepository extends JpaRepository<UncomfortableDomain, Long>, UncomfortableCustomRepository{
@@ -16,16 +13,4 @@ public interface UncomfortableRepository extends JpaRepository<UncomfortableDoma
     @Query(value = "SELECT COUNT(table.uncomfortableIdx) " +
             "FROM UncomfortableDomain table" )
     Long amountUncomfortable();
-
-    @Query("SELECT new com.moment.the.uncomfortable.dto.UncomfortableResponseDto(table.uncomfortableIdx, table.content, table.goods, answer)" +
-            "FROM UncomfortableDomain table LEFT JOIN table.answerDomain answer " +
-            "ORDER BY table.uncomfortableIdx DESC "
-    )
-    List<UncomfortableResponseDto> uncomfortableViewAll();
-
-    @Query("SELECT new com.moment.the.uncomfortable.dto.UncomfortableResponseDto(table.uncomfortableIdx, table.content, table.goods, answer)" +
-            "FROM UncomfortableDomain table LEFT JOIN table.answerDomain answer " +
-            "ORDER BY table.goods DESC "
-    )
-    List<UncomfortableResponseDto> uncomfortableViewTopBy(Pageable p);
 }
