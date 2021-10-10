73c1249d688123a651e0b67acd4d659b83df3526
johnjihwan <s20062@gsm.hs.kr>
2021-10-10 19:36:45 +0900

    Add @EnableScheduling to enable scheduling method

--- src/main/java/com/moment/the/TheApplication.java
+++ src/main/java/com/moment/the/TheApplication.java
@@ -3,9 +3,11 @@ package com.moment.the;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class TheApplication {
 
 	public static void main(String[] args) {
