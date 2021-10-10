f6671fd3ae7a5b1d5bcdec2ac9512b1e32cb02b2
johnjihwan <s20062@gsm.hs.kr>
2021-10-10 20:36:28 +0900

    Add springboot app을 한국 표준시로 시작하도록 설정

--- src/main/java/com/moment/the/TheApplication.java
+++ src/main/java/com/moment/the/TheApplication.java
@@ -1,15 +1,29 @@
 package com.moment.the;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@Slf4j
 public class TheApplication {
-
+	/**
+	 * springboot를 한국 표준시로 설정합니다.
+	 * `@PostConstruct`를 사용하여 메소드를 단 한번만 호출 되도록 합니다.
+	 */
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		log.info("========== server start time: {}", new Date());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TheApplication.class, args);
 	}
