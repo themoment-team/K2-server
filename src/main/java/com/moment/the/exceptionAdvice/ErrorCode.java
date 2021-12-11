eaef38a4be91229d1bc4301ef2438ecd638b3932
johnjihwan <s20062@gsm.hs.kr>
2021-12-11 20:39:53 +0900

    Add Error code 모아두는 Enum class 추가

--- src/main/java/com/moment/the/exceptionAdvice/ErrorCode.java
+++ src/main/java/com/moment/the/exceptionAdvice/ErrorCode.java
@@ -1,2 +1,15 @@
-package com.moment.the.exceptionAdvice;public enum ErrorCode {
+package com.moment.the.exceptionAdvice;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum ErrorCode {
+    BAD_REQUEST(400, ErrorClassification.COMMON+"-ERR-400", "Bad Request"),
+    UNAUTHORIZED(401, ErrorClassification.COMMON+"-ERR-401", "Unauthorized"),
+    FORBIDDEN(403, ErrorClassification.COMMON+"-ERR-403", "Forbidden"),
+    ;
+
+    private int status;
+    private String errorCode;
+    private String message;
 }
