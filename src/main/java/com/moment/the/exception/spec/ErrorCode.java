f16f4374e4f81b0ed65a1edd6e1915a044550752
johnjihwan <s20062@gsm.hs.kr>
2021-12-11 21:39:45 +0900

    Add Exception ErrorCode 정의 항목 추가

--- src/main/java/com/moment/the/exception/spec/ErrorCode.java
+++ src/main/java/com/moment/the/exception/spec/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
     BAD_REQUEST(400, "Bad Request",ErrorClassification.COMMON+"-ERR-400"),
     UNAUTHORIZED(401, "Unauthorized",ErrorClassification.COMMON+"-ERR-401"),
     FORBIDDEN(403,  "Forbidden",ErrorClassification.COMMON+"-ERR-403"),
+    EMAIL_DUPLICATION(400, "Email Duplicated", ErrorClassification.ADMIN+"-ERR-400"),
     ;
 
     private int status;
