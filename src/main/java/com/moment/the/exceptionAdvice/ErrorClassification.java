2d2865eb97e8c943076bdd076bde924f2c1693a8
johnjihwan <s20062@gsm.hs.kr>
2021-12-11 20:36:24 +0900

    Add 번외적인 테스트

---
+++ src/main/java/com/moment/the/exceptionAdvice/ErrorClassification.java
@@ -0,0 +1,2 @@
+package com.moment.the.exceptionAdvice;public enum ErrorClassification {
+}
--- src/test/java/com/moment/the/TheApplicationTests.java
+++ src/test/java/com/moment/the/TheApplicationTests.java
@@ -1,9 +1,20 @@
 package com.moment.the;
 
+import com.moment.the.exceptionAdvice.ErrorClassification;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
+@Slf4j
 class TheApplicationTests {
+    @Test
+    @DisplayName("번외 - Enum과 String 합치기 테스트")
+    void str_compare_test(){
+        String str = ErrorClassification.COMMON + "-ERR-400";
+        log.info("========="+str);
+    }
 }
