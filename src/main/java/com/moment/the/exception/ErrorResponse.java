db7910513bcf51725bd724a740cd133d15954659
johnjihwan <s20062@gsm.hs.kr>
2021-12-12 13:13:45 +0900

    Add annotation

--- src/main/java/com/moment/the/exception/ErrorResponse.java
+++ src/main/java/com/moment/the/exception/ErrorResponse.java
@@ -3,6 +3,12 @@ package com.moment.the.exception;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * client에게 반환할 payload 클래스
+ *
+ * @version 1.3
+ * @author 전지환
+ */
 @Getter
 @Setter
 public class ErrorResponse {
