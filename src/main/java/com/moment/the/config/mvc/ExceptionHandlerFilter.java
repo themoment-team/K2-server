f5ec7cb0d2c4719ee6849f6f7965462153b56db4
jjo <k01066624566@gmail.com>
2021-12-14 22:16:34 +0900

    fix ExceptionHandlerFilter

--- src/main/java/com/moment/the/config/mvc/ExceptionHandlerFilter.java
+++ src/main/java/com/moment/the/config/mvc/ExceptionHandlerFilter.java
@@ -42,42 +42,9 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
         try {
             filterChain.doFilter(request, response);
-        }catch(InvalidTokenException e){
-            responseExceptionMessage(HttpStatus.BAD_REQUEST, response, exceptionAdvice.invalidToken(request, e));
-        }catch(AccessTokenExpiredException e){
-            responseExceptionMessage(HttpStatus.BAD_REQUEST, response, exceptionAdvice.accessTokenExpiredException(request, e));
-        }catch (UserNotFoundException e){
-            responseExceptionMessage(HttpStatus.BAD_REQUEST, response, exceptionAdvice.userNotFoundException(request, e));
         }catch (Exception e){
             log.error("알 수 없는 에러 발생", e);
-            responseExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR, response, exceptionAdvice.defaultException(request, e));
-        }
-    }
-
-    /**
-     * filter에서 유저에게 Exception메시지를 전송하는 메서드
-     * @param status HttpStatus
-     * @param response HttpServletResponse
-     * @param exceptionResult 해당 Exceptoin에 대한 Client에 반환할 정보를 가지고 있는 CommonResult객체
-     * @author 정시원
-     */
-    private void responseExceptionMessage(HttpStatus status, HttpServletResponse response, CommonResult exceptionResult) {
-        response.setStatus(status.value());
-        response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
-
-        int exceptionCode = exceptionResult.getCode();
-        String exceptionMsg = exceptionResult.getMsg();
-
-        try {
-            String exceptionResultToJson = responseService.getFailResultConvertString(exceptionCode, exceptionMsg); // CommonResult에 있는 값을 json으로 변환
-            log.debug(
-                    "filter에서 Exception 발생 원인: \n {}",
-                    exceptionResultToJson
-            );
-            response.getWriter().write(exceptionResultToJson); // filter 단에서 client에 json를 보넨다.
-        } catch (IOException e) {
-            log.error("Filter에서 Error Response Json변환 실패", e);
-            throw new RuntimeException(); // 알 수 없는 에러를 위해 일단 RuntimeException을 발생시킴
+//            responseExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR, response, exceptionAdvice.defaultException(request, e));
         }
     }
 }
