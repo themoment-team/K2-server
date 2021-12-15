6a18b9ac0a581f91ae87be2760a25c99ed31b3fa
johnjihwan <s20062@gsm.hs.kr>
2021-12-15 11:26:04 +0900

    Udpate docker build 시에 cache 비활성화

--- prod-build.sh
+++ prod-build.sh
@@ -7,7 +7,7 @@ echo "====gradle clean build======"
 ./gradlew clean build
 
 echo "======docker-build======="
-docker build . -t the-moment-server
+docker build --no-cache . -t the-moment-server
 
 echo "========docker run========"
 docker run -it -p 5000:8080 --name the-moment-app the-moment-server
