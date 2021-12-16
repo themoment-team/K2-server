d48ac7d7a09c8dd2464ed975a33dd8e1af47f09a
johnjihwan <s20062@gsm.hs.kr>
2021-12-16 11:29:13 +0900

    Update jwt 를 없앤 login 로직

--- src/main/java/com/moment/the/admin/service/AdminService.java
+++ src/main/java/com/moment/the/admin/service/AdminService.java
@@ -1,13 +1,12 @@
 package com.moment.the.admin.service;
 
+import com.moment.the.admin.AdminDomain;
 import com.moment.the.admin.dto.AdminDto;
 import com.moment.the.admin.dto.SignInDto;
 
-import java.util.Map;
-
 public interface AdminService {
     void join(AdminDto adminDto) throws Exception;
-    Map<String, String> login(String id, String password) throws Exception;
+    AdminDomain login(SignInDto signInDto) throws Exception;
     void logout();
     void withdrawal(SignInDto SignInDto) throws Exception;
 }
--- src/main/java/com/moment/the/admin/service/AdminServiceImpl.java
+++ src/main/java/com/moment/the/admin/service/AdminServiceImpl.java
@@ -15,9 +15,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 @Service
 @RequiredArgsConstructor
@@ -38,21 +35,16 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public Map<String, String> login(String id, String password) {
+    public AdminDomain login(SignInDto signInDto) {
         // 아이디 검증
-        AdminDomain adminDomain = adminRepository.findByEmail(id);
+        AdminDomain adminDomain = adminRepository.findByEmail(signInDto.getEmail());
         if (adminDomain == null) throw new UserNotFoundException();
+
         // 비밀번호 검증
-        boolean passwordCheck = passwordEncoder.matches(password, adminDomain.getPassword());
+        boolean passwordCheck = passwordEncoder.matches(signInDto.getPassword(), adminDomain.getPassword());
         if (!passwordCheck) throw new UserNotFoundException();
 
-        final String accessToken = jwtUtil.generateAccessToken(adminDomain.getEmail());
-        // token 만료 기간 설정
-        Map<String ,String> map = new HashMap<>();
-        map.put("id", adminDomain.getEmail());
-        map.put("accessToken", accessToken); // accessToken 반환
-
-        return map;
+        return adminDomain;
     }
 
     // 로그아웃
