4f6e6e453bedc9bcc6190beef6f326ae9606d5c0
johnjihwan <s20062@gsm.hs.kr>
2021-12-16 11:28:51 +0900

    Add session 상수 interface 로 분리

---
+++ src/main/java/com/moment/the/config/security/session/SessionConstants.java
@@ -0,0 +1,11 @@
+package com.moment.the.config.security.session;
+
+/**
+ * 세션의 상수를 interface로 분리해 객체 생성을 막는다.
+ *
+ * @version 1.4
+ * @author 전지환
+ */
+public interface SessionConstants {
+    String LOGIN_ADMIN = "loginAdmin";
+}
