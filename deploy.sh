8788e62b2d673523593064e5db53fc0cd90f2c21
lot8229 <lot8229@kakao.com>
2021-12-21 11:05:06 +0900

    Change Branche to Master

--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,5 @@ fi
 echo "> $JAR_PATH 배포"
 sudo nohup java -jar $JAR_PATH -Djava.security.egd=file:/dev/./urandom &
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 echo "> $CURRENT_PID 에서 실행중"
