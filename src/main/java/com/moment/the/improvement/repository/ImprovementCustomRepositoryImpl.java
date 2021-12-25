f6076061a33cae297eb1096504e9a601df0acbb9
johnjihwan <s20062@gsm.hs.kr>
2021-12-25 23:35:12 +0900

    Add 주석

--- src/main/java/com/moment/the/improvement/repository/ImprovementCustomRepositoryImpl.java
+++ src/main/java/com/moment/the/improvement/repository/ImprovementCustomRepositoryImpl.java
@@ -2,7 +2,6 @@ package com.moment.the.improvement.repository;
 
 import com.moment.the.improvement.dto.ImprovementDto;
 import com.moment.the.improvement.dto.QImprovementDto_Response;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +11,23 @@ import java.util.List;
 
 import static com.moment.the.improvement.QImprovementDomain.improvementDomain;
 
+/**
+ * 실제개선사례 customRepository
+ *
+ * @version 1.3.1
+ * @author 전지환
+ */
 @RequiredArgsConstructor
 public class ImprovementCustomRepositoryImpl implements ImprovementCustomRepository{
 
     private final JPAQueryFactory queryFactory;
 
+    /**
+     * 실제개선사례 전체 조회 쿼리 메소드
+     *
+     * @return List<ImprovementDto.Response>
+     * @author 전지환
+     */
     @Override
     @Transactional(readOnly = true)
     public List<ImprovementDto.Response> getAllImprovement() {
