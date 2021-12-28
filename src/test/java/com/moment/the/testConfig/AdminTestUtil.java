dbd741cc7a399477881535577ba487a3728b6421
johnjihwan <s20062@gsm.hs.kr>
2021-12-28 16:50:14 +0900

    Delete Aspect 어노테이션 삭제

--- src/test/java/com/moment/the/testConfig/AdminTestUtil.java
+++ src/test/java/com/moment/the/testConfig/AdminTestUtil.java
@@ -4,7 +4,6 @@ import com.moment.the.admin.AdminDomain;
 import com.moment.the.admin.repository.AdminRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 import java.util.Collections;
 import java.util.List;
 
-@Aspect
 @Component
 @Slf4j
 public class AdminTestUtil {
