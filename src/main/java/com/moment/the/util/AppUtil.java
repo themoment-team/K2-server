9b1c8d9faed28d3dbb6c5267816369687dcf0063
johnjihwan <s20062@gsm.hs.kr>
2021-12-28 16:25:36 +0900

    Update 애플리케이션에서 전역으로 사용되는 method 모아놓음

--- src/main/java/com/moment/the/util/AppUtil.java
+++ src/main/java/com/moment/the/util/AppUtil.java
@@ -1,2 +1,30 @@
-package com.moment.the.util;public class AppUtil {
+package com.moment.the.util;
+
+import com.moment.the.admin.AdminDomain;
+import com.moment.the.admin.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AppUtil {
+
+    private final AdminRepository adminRepository;
+
+    public static String getCurrentAdminEmail(){
+        String userEmail;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails) {
+            userEmail = ((UserDetails) principal).getUsername();
+        } else {
+            userEmail = principal.toString();
+        }
+        return userEmail;
+    }
+
+    public AdminDomain getCurrentAdminEntity(){
+        return adminRepository.findByEmail(getCurrentAdminEmail());
+    }
 }
