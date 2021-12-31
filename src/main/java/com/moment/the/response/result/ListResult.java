aacabb09c500a4c4495e4c1d0e68a6a9b285034e
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-12-31 13:43:23 +0900

    fix response

--- src/main/java/com/moment/the/response/result/ListResult.java
+++ src/main/java/com/moment/the/response/result/ListResult.java
@@ -1,5 +1,6 @@
 package com.moment.the.response.result;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +9,6 @@ import java.util.List;
 @Getter
 @Setter
 public class ListResult<T> extends CommonResult {
+    @ApiModelProperty(value = "응답 데이터")
     private List<T> list;
 }
--- src/main/java/com/moment/the/response/result/SingleResult.java
+++ src/main/java/com/moment/the/response/result/SingleResult.java
@@ -1,10 +1,12 @@
 package com.moment.the.response.result;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SingleResult<T> extends CommonResult {
+    @ApiModelProperty(value = "응답 데이터")
     private T data;
 }
