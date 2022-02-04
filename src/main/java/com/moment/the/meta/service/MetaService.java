517c0c614993ab7b5ffdd89bb31cb722520f853c
johnjihwan <s20062@gsm.hs.kr>
2022-02-04 19:17:01 +0900

    Udpate uncomfortableService/calculateTermMethod

--- src/main/java/com/moment/the/meta/service/MetaService.java
+++ src/main/java/com/moment/the/meta/service/MetaService.java
@@ -1,2 +1,31 @@
-package com.moment.the.meta;public class MetaService {
+package com.moment.the.meta.service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+@Service
+public class MetaService {
+    /**
+     * 프로젝트 D-day를 세어 가져옵니다.
+     * @return int
+     */
+    public int getTermProjectStart(){
+        return calculateAfterDate();
+    }
+
+    /**
+     * D-day를 계산하는 메서드.
+     * @return int - D-day after
+     */
+    private static int calculateAfterDate() {
+        //  today: 오늘 날짜
+        //  theMomentStart: the-moment 시작 날짜
+        LocalDate today = LocalDate.now();
+        LocalDate theMomentStart = LocalDate.of(2021, 6, 7);
+
+        // the_moment 프로젝트를 시작한 날짜 by 오늘의 날짜
+        return (int) theMomentStart.until(today, ChronoUnit.DAYS);
+    }
 }
