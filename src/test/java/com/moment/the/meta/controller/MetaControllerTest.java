386c98511fd240a866f71ac92ec4fb953d081a8b
johnjihwan <s20062@gsm.hs.kr>
2022-02-05 15:53:29 +0900

    Add Meta/ application meta data를 제공하는 controller 테스트

--- src/test/java/com/moment/the/meta/controller/MetaControllerTest.java
+++ src/test/java/com/moment/the/meta/controller/MetaControllerTest.java
@@ -1,21 +1,32 @@
 package com.moment.the.meta.controller;
 
+import com.moment.the.meta.service.MetaService;
 import com.moment.the.testConfig.AbstractControllerTest;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
-public class CatchError extends AbstractControllerTest {
+@Slf4j
+public class MetaControllerTest extends AbstractControllerTest {
 
     @Autowired
     private MetaController metaController;
+
+    @Autowired
+    private MetaService metaService;
 
     @Override
     protected Object controller() {
@@ -23,8 +34,28 @@ public class CatchError extends AbstractControllerTest {
     }
 
     @Test
-    @DisplayName("mocking controller")
+    @DisplayName("project term을 조회하면 mocking된 날짜를 기준으로 term이 조회된다.")
     void mocking_controller() throws Exception {
+
+        LocalDate prefixLocalDate = LocalDate.of(2022, 1, 25);
+
+        // mocking:: prefixLocalDate를 LocalDate.now 를 호출할 때 반환한다.
+        try(final MockedStatic<LocalDate> localDateMockedStatic = Mockito.mockStatic(LocalDate.class)){
+            localDateMockedStatic.when(LocalDate::now).thenReturn(prefixLocalDate);
+
+            log.info("======= LocalDate.now(): {} ======== prefixLocalDate: {} ", LocalDate.now(), prefixLocalDate);
+            assertEquals(LocalDate.now(), prefixLocalDate);
+
+            final int metaServiceTermValue = metaService.getTermProjectStart();
+            final int mockingTermValue = MetaService.calculateAfterDate();
+
+            log.info("======== metaService logic result: {} ========= mockTest logic result: {}", metaServiceTermValue, mockingTermValue);
+            assertEquals(metaServiceTermValue, mockingTermValue);
+
+        } catch (Exception e){
+            System.out.println("e = " + e);
+        }
+
         this.mvc.perform(get("/meta/v1.3.1/term")).andExpect(status().isOk());
     }
 }
