dfd3d9d981cec1578a2ff60cf7cc4af6224d8718
johnjihwan <s20062@gsm.hs.kr>
2022-02-09 19:24:23 +0900

    Add Spring PSA pattern

--- src/main/java/com/moment/the/meta/service/MetaService.java
+++ src/main/java/com/moment/the/meta/service/MetaService.java
@@ -7,13 +7,45 @@ import java.time.temporal.ChronoUnit;
 
 @Service
 public class MetaService {
+
+    /**
+     * Use spring PSA:: static method mockito로 테스트하기 까다롭다.
+     *
+     * @author 전지환
+     */
+    @Service
+    interface LocalDateService {
+        LocalDate getLocalDateNow();
+        LocalDate getReleaseDate();
+    }
+
+    @Service
+    static class MetaLocalDateService implements LocalDateService{
+        @Override
+        public LocalDate getLocalDateNow() {
+            return LocalDate.now();
+        }
+
+        @Override
+        public LocalDate getReleaseDate() {
+            return LocalDate.of(2021, 6, 7);
+        }
+    }
+
+    private final LocalDateService localDateService;
+
+    // LocalDateService로 추상화 타입을 잡고 MetaLocalDateService를 주입한다.
+    public MetaService(MetaLocalDateService metaLocalDateService) {
+        this.localDateService = metaLocalDateService;
+    }
+
     /**
      * 오늘을 기준으로 프로젝트 시행 기간을 가져온다.
      *
      * @return int - termProjectStart
      */
     public int getTermProjectStart(){
-        return calculateAfterDate(LocalDate.now());
+        return calculateAfterDate(localDateService.getLocalDateNow());
     }
 
     /**
@@ -22,11 +54,11 @@ public class MetaService {
      * @param today 오늘 날짜
      * @return int - D-day after
      */
-    public static int calculateAfterDate(LocalDate today) {
-        //  theMomentStart: the-moment 시작 날짜
-        LocalDate theMomentStart = LocalDate.of(2021, 6, 7);
+    private int calculateAfterDate(LocalDate today) {
+        //  releaseDate: every-moment 출시일
+        final LocalDate releaseDate = localDateService.getReleaseDate();
 
-        // the_moment 프로젝트를 시작한 날짜 by 오늘의 날짜
-        return (int) theMomentStart.until(today, ChronoUnit.DAYS);
+        // every-moment 출시일 by 오늘
+        return (int) releaseDate.until(today, ChronoUnit.DAYS);
     }
 }
